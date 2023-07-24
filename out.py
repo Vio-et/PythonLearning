0d01b0c84d8f73f14611e02c7c5e09034dd632b2
luck-kdt <1669785222@qq.com>
2023-07-24 12:06:49 +0800

    push out.py

---
+++ out.py
@@ -0,0 +1,12 @@
+# @Orlando Hello World
+
+print("Hello World")
+
+age = 18
+name = 'Orlando'
+weight = 69.5
+studentID = 150
+print("My age is %d." % age)
+print("My name is %s." % name)
+print("My weight is %.2f" % weight)
+print("My student ID is %u" % studentID)
