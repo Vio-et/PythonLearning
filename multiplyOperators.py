db86f1e7df8cbbc662b1b82ac1586d8082856400
luck-kdt <1669785222@qq.com>
2023-08-28 13:50:59 +0800

    if chapter commit

--- multiplyOperators.py
+++ multiplyOperators.py
@@ -1,4 +1,4 @@
-# Python运算符
+# Python运算符 -- () 高于 ** 高于 * / // % 高于 +-
 num, float1, str1 = 10, 5.6, 'Hello Orlando'
 print(num)
 print(float1)
@@ -20,3 +20,6 @@ three = 3
 print((one < two) and (three > two))
 print((one >= two) or (three <= two))
 print(not ((one < two) and (three > two)))
+
+# 数字之间的and： 若有一个为0，则为0；否则返回最后一个非0数字
+# 数字之间的or： 只有所有值为0，则为0；否则返回第一个非0数字
